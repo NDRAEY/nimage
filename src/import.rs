d516305e051869695afa085408b34a3326e28db3
Andrey <pikachu_andrey@vk.com>
2025-04-24 14:48:06 +0900

    Add open

--- src/import.rs
+++ src/import.rs
@@ -1,12 +1,19 @@
-pub fn open(data: &[u8]) -> Option<Image> {
+use crate::Image;
+
+/// Tries to open file using various formats. Returns `None` if no format matches given data.
+pub fn open(data: &[u8]) -> Option<(&'static str, Image)> {
     #[cfg(feature = "png")]
     {
-        return crate::png::from_png_data(data);
+        if let Some(image) = crate::png::from_png_data(data) {
+            return Some(("png", image));
+        }
     }
 
     #[cfg(feature = "tga")]
     {
-        return crate::tga::from_tga_data(data);
+        if let Some(image) = crate::tga::from_tga_data(data) {
+            return Some(("tga", image));
+        }
     }
 
     None
